006df47072e8fb3fc381954fefa0799161f8c472
Alejandro <amillerg@buffalo.edu>
2019-05-08 15:29:42 -0400

    converted model functions from python to scala

---
+++ src/ScalaClasses/modelFunctions.scala
@@ -0,0 +1,41 @@
+package ScalaClasses
+
+import MVC.model.physics.PhysicsVector
+
+import scala.collection.immutable.ListMap
+
+object modelFunctions {
+  
+
+  def buy_life(mouse: PhysicsVector): Boolean={
+    var x: Double = mouse.x
+    var y: Double = mouse.y
+    if(x<1150 & x>100 & y>350 & y<420){
+      true
+    }else{
+      false
+    }
+  }
+
+  def buy_a_life(ship: Player): Unit={
+    ship.health= ship.health + 1
+    ship.coins -= 200
+  }
+
+  def topPlayers(playerList: List[Player]): List[String] ={ //takes list of all players, returns list of usernames from first to last place
+    var top: List[String] = List()
+    var kds: Map[String, Double] = Map()
+    var kd: scala.collection.immutable.Map[String,Double] = kds
+    var ratio: Double = 0.0
+    for(p<-playerList){
+      ratio = p.kills/p.deaths
+      kds += (p.user -> ratio)
+    }
+    var x = ListMap(kd.toSeq.sortWith(_._2 > _._2):_*)
+    for(i<-x.keys){
+      top :+= i
+    }
+    top
+  }
+
+}
