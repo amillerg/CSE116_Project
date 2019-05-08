cb63b0efba0676bb60f7149bd4e9b1406a0265f3
Alejandro <amillerg@buffalo.edu>
2019-05-08 17:29:14 -0400

    edited game class

--- src/MVC/model/Game.scala
+++ src/MVC/model/Game.scala
@@ -2,17 +2,19 @@ package MVC.model
 
 import MVC.model.physics.{PhysicalObject, World}
 import play.api.libs.json.{JsValue, Json}
-
+import scala.collection.immutable.ListMap
+import ScalaClasses._
+import physics._
 
 class Game {
 
 
   var world : World = new World(10)
 
-  var projectiles: List[PhysicalObject] = List()
+  var projectiles: List[Projectile] = List()
 
   var players: Map[String, Player] = Map()
-  val playerSize: Double = 0.3
+  val playerSize: Double = 32
 
   var lastUpdateTime: Long = System.nanoTime()
 
@@ -29,9 +31,6 @@ class Game {
     players(id).destroy()
     players -= id
   }
-
-
-
 
 
   def addProjectile(projectile: PhysicalObject): Unit = {
@@ -110,9 +109,79 @@ class Game {
       }
     }
   }
+
+
+
   def checkForPlayerHits(): Unit = {
+    val vec: PhysicsVector = startingVector()
+    for(i<-projectiles){
+      var proj: PhysicsVector = new PhysicsVector(i.location.x, i.location.y, 0)
+      for(p<-players.values){
+        var dist: Double = p.location.distance2d(proj)
+        if(dist < playerSize){
+          p.location.x = vec.x
+          p.location.y = vec.y
+          i.destroy()
+        }
+      }
+    }
     // TODO: Objective 3
-    players.values.foreach(player=>hit(player))
+    //players.values.foreach(player=>hit(player))
+  }
+
+  def checkHit(): Unit={
+      for(b<-projectiles) {
+        var proj: PhysicsVector = new PhysicsVector(b.location.x, b.location.y)
+        for (p <- players.values){
+          var dist: Double = p.location.dist(proj)
+          if(dist<playerSize){
+            p.health -= 1
+            if(p.health == 0){ //if the player is killed
+              p.deaths += 1
+              p.coins = 0
+              p.health = 3
+              p.location.x = 600 //resets location to starting point
+              p.location.y = 300
+              var shooter: Player = players(b.user)
+              shooter.coins += 100
+              shooter.kills += 1
+            }
+          }
+        }
+      }
+  }
+
+
+
+  def buy_life(mouse: PhysicsVector): Boolean={
+    var x: Double = mouse.x
+    var y: Double = mouse.y
+    if(x<1150 & x>100 & y>350 & y<420){
+      true
+    }else{
+      false
+    }
+  }
+
+  def buy_a_life(ship: Player): Unit={
+    ship.health= ship.health + 1
+    ship.coins -= 200
+  }
+
+  def topPlayers(playerList: Map[String, Player]): List[String] ={ //takes list of all players, returns list of usernames from first to last place
+    var top: List[String] = List()
+    var kds: Map[String, Double] = Map()
+    var kd: scala.collection.immutable.Map[String,Double] = kds
+    var ratio: Double = 0.0
+    for(p<-playerList.values){
+      ratio = p.kills/p.deaths
+      kds += (p.user -> ratio)
+    }
+    var x = ListMap(kd.toSeq.sortWith(_._2 > _._2):_*)
+    for(i<-x.keys){
+      top :+= i
+    }
+    top
   }
 
 
